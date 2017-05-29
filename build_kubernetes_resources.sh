75996a47fc851eb8203e8586fa5259b1b68e5b17
Silin Na <n1207n@gmail.com>
2017-05-29 13:02:10 -0700

    feat: Created kubernetes resources for nginx

--- build_kubernetes_resources.sh
+++ build_kubernetes_resources.sh
@@ -3,4 +3,7 @@ kubectl create -f kubernetes/secrets/redis-queue-info.yml
 kubectl create -f kubernetes/video-api-deployment.yml
 kubectl create -f kubernetes/video-api-service.yml
 
+kubectl create -f kubernetes/nginx-deployment.yml
+kubectl create -f kubernetes/nginx-service.yml
+
 kubectl create -f kubernetes/queue-consumer-job.yml
