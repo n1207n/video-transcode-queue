a570f8ebf16511277bcc3eee84d98ea2ffddbe59
Silin Na <n1207n@gmail.com>
2017-05-29 12:24:37 -0700

    feat: Added REST API logics
    + getVideoList
    
    + getVideoDetail
    
    + createVideo
    
    + uploadVideoFile

--- video_backend_api/api_server.go
+++ video_backend_api/api_server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-pg/pg"
 	"github.com/go-pg/pg/orm"
 	"github.com/golang/glog"
+	"gopkg.in/gin-gonic/gin.v1"
 )
 
 var (
@@ -23,6 +24,8 @@ func main() {
 		glog.Infoln("Failed to sync up database tables. Check the connection.")
 		panic(err)
 	}
+
+	startAPIServer()
 }
 
 // loadEnvironmentVariables loads PostgreSQL
@@ -81,4 +84,37 @@ func createSchemas(db *pg.DB) error {
 	}
 
 	return nil
+}
+
+func startAPIServer() {
+	// Creates a gin router with default middleware:
+	// logger and recovery (crash-free) middleware
+	router := gin.Default()
+
+	v1 := router.Group("/v1")
+	{
+		v1.GET("/videos", getVideoList)
+		v1.GET("/videos/:id", getVideoDetail)
+		v1.POST("/videos", createVideo)
+		v1.POST("/video-upload", uploadVideoFile)
+	}
+
+	// By default it serves on :8080
+	router.Run()
+}
+
+func getVideoList(c *gin.Context) {
+
+}
+
+func getVideoDetail(c *gin.Context) {
+
+}
+
+func createVideo(c *gin.Context) {
+
+}
+
+func uploadVideoFile(c *gin.Context) {
+
 }
