d8fc130c6e86b1dd71a9fa97405d419208c35d91
Silin Na <n1207n@gmail.com>
2017-05-30 22:23:33 -0700

    refactor: Repurposed api_server.go for transcoder_service
    + Created Kubernetes resources

--- transcoder_service/transcode_api_server.go
+++ transcoder_service/transcode_api_server.go
@@ -2,94 +2,14 @@ package main
 
 import (
 	"net/http"
-	"os"
-	"strconv"
 
-	"github.com/go-pg/pg"
-	"github.com/go-pg/pg/orm"
 	"github.com/golang/glog"
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
-var (
-	pgDb, pgUser, pgPassword, pgHost string
-	queueTopic                       string
-)
-
 func main() {
-	loadEnvironmentVariables()
-
-	err := createSchemas()
-	if err != nil {
-		glog.Infoln("Failed to sync up database tables. Check the connection.")
-		panic(err)
-	}
-
+	glog.Infoln("Starting transcoder API server")
 	startAPIServer()
-}
-
-// loadEnvironmentVariables loads PostgreSQL
-// information from dotenv
-func loadEnvironmentVariables() {
-	pgDb = os.Getenv("PGDB")
-	if len(pgDb) == 0 {
-		panic("No PGDB environment variable")
-	}
-
-	pgUser = os.Getenv("PGUSER")
-	if len(pgUser) == 0 {
-		panic("No PGUSER environment variable")
-	}
-
-	pgPassword = os.Getenv("PGPASSWORD")
-	if len(pgPassword) == 0 {
-		panic("No PGPASSWORD environment variable")
-	}
-
-	pgHost = os.Getenv("PGHOST")
-	if len(pgHost) == 0 {
-		panic("No PGHOST environment variable")
-	}
-
-	queueTopic = os.Getenv("QUEUE_TOPIC")
-	if len(queueTopic) == 0 {
-		panic("No QUEUE_TOPIC environment variable")
-	}
-}
-
-// getDatabaseConnection returns an instance
-// as a database connection
-func getDatabaseConnection() *pg.DB {
-	db := pg.Connect(&pg.Options{
-		User:     pgUser,
-		Password: pgPassword,
-		Database: pgDb,
-		Addr:     pgHost,
-	})
-
-	return db
-}
-
-// createSchemas creates a set of database tables
-// from Go struct classes
-func createSchemas() error {
-	var db *pg.DB = getDatabaseConnection()
-
-	defer func() {
-		db.Close()
-	}()
-
-	for _, model := range []interface{}{&VideoRendering{}, &Video{}} {
-		err := db.CreateTable(model, &orm.CreateTableOptions{
-			IfNotExists: true,
-		})
-
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 func startAPIServer() {
@@ -99,50 +19,30 @@ func startAPIServer() {
 
 	v1 := router.Group("/v1")
 	{
-		v1.GET("/videos", getVideoList)
-		v1.GET("/videos/:id", getVideoDetail)
-		v1.POST("/videos", createVideo)
-		v1.POST("/video-upload", uploadVideoFile)
+		v1.POST("/video-transcode", transcodeVideo)
 	}
 
 	// By default it serves on :8080
 	router.Run()
 }
 
-func getVideoList(c *gin.Context) {
-	count, videos, err := GetVideoObjects()
+func transcodeVideo(c *gin.Context) {
+	var request TranscodeRequest
 
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-	} else {
-		c.JSON(http.StatusOK, gin.H{
-			"count":   count,
-			"results": videos,
-		})
+	if c.BindJSON(&request) == nil {
+		// TODO: Check if video id exists in PostgreSQL
+
+		performTranscoding(request.Path)
+		c.JSON(http.StatusAccepted, gin.H{"video_id": request.VideoID})
 	}
 }
 
-func getVideoDetail(c *gin.Context) {
-	videoID, err := strconv.Atoi(c.Param("id"))
-
-	video, err := GetVideoObject(videoID)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-	} else {
-		c.JSON(http.StatusOK, gin.H{
-			"data": video,
-		})
-	}
+// TranscodeRequest represents a JSON POST data for video-transcode API
+type TranscodeRequest struct {
+	Path    string `json:"path"`
+	VideoID string `json:"video_id"`
 }
 
-func createVideo(c *gin.Context) {
-
-}
-
-func uploadVideoFile(c *gin.Context) {
+func performTranscoding(filePath string) {
 
 }
