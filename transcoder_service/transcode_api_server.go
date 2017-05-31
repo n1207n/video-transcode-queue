04c5e3bf26682efeb6f3683be22e4910b5eed053
Silin Na <n1207n@gmail.com>
2017-05-30 22:47:45 -0700

    feat: Added joy4 transcoding logic

--- transcoder_service/transcode_api_server.go
+++ transcoder_service/transcode_api_server.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/golang/glog"
+	"github.com/nareix/joy4/av"
+	"github.com/nareix/joy4/av/avutil"
+	"github.com/nareix/joy4/av/transcode"
+	"github.com/nareix/joy4/cgo/ffmpeg"
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
@@ -32,8 +37,13 @@ func transcodeVideo(c *gin.Context) {
 	if c.BindJSON(&request) == nil {
 		// TODO: Check if video id exists in PostgreSQL
 
-		performTranscoding(request.Path)
-		c.JSON(http.StatusAccepted, gin.H{"video_id": request.VideoID})
+		transcodedFilePaths, err := performTranscoding(request.Path)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("Failed to open a video file: %s for Video ID %s", request.Path, request.VideoID)})
+		} else {
+			c.JSON(http.StatusAccepted, gin.H{"video_id": request.VideoID})
+		}
 	}
 }
 
@@ -43,6 +53,42 @@ type TranscodeRequest struct {
 	VideoID string `json:"video_id"`
 }
 
-func performTranscoding(filePath string) {
+func performTranscoding(filePath string) (transcodedFilePaths []string, transcodeError error) {
+	infile, err := avutil.Open(filePath)
 
+	if err != nil {
+		transcodeError = err
+	}
+
+	encoded_audio_info := func(stream av.AudioCodecData, i int) (need bool, dec av.AudioDecoder, enc av.AudioEncoder, err error) {
+		need = true
+		dec, _ = ffmpeg.NewAudioDecoder(stream)
+		enc, _ = ffmpeg.NewAudioEncoderByName("libfdk_aac")
+		enc.SetSampleRate(stream.SampleRate())
+		enc.SetChannelLayout(av.CH_STEREO)
+		enc.SetBitrate(12000)
+		enc.SetOption("profile", "HE-AACv2")
+		return
+	}
+
+	transcode_demuxer := &transcode.Demuxer{
+		Options: transcode.Options{
+			FindAudioDecoderEncoder: encoded_audio_info,
+		},
+		Demuxer: infile,
+	}
+
+	outfile, err := avutil.Create("output.ts")
+
+	if err != nil {
+		transcodeError = err
+	} else {
+		avutil.CopyFile(outfile, trans)
+
+		outfile.Close()
+		infile.Close()
+		transcode_demuxer.Close()
+	}
+
+	return
 }
