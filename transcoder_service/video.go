d66d8a7f82b4a315e8ab446eea20040ccb065954
Silin Na <n1207n@gmail.com>
2017-06-02 18:20:35 -0700

    refactor: Added notnull annotation to fields in Video models

--- transcoder_service/video.go
+++ transcoder_service/video.go
@@ -5,12 +5,12 @@ type VideoRendering struct {
 	tableName struct{} `sql:"video_renderings, alias:video_rendering"`
 
 	ID             string `json:"id"`
-	RenderingTitle string `json:"rendering_title"`
+	RenderingTitle string `sql:",notnull",json:"rendering_title"`
 
-	FilePath string `json:"file_path"`
-	URL      string `json:"url"`
-	Width    uint   `json:"width"`
-	Height   uint   `json:"height"`
+	FilePath string `sql:",notnull",json:"file_path"`
+	URL      string `sql:",notnull",json:"url"`
+	Width    uint   `sql:",notnull",json:"width"`
+	Height   uint   `sql:",notnull",json:"height"`
 
 	VideoID int
 }
@@ -22,8 +22,8 @@ type Video struct {
 	tableName struct{} `sql:"videos, alias:video"`
 
 	ID             string `json:"id"`
-	Title          string `json:"title"`
-	IsReadyToServe bool   `json:"is_ready_to_serve"`
+	Title          string `sql:",notnull",json:"title"`
+	IsReadyToServe bool   `sql:",notnull",json:"is_ready_to_serve"`
 
 	Renders []*VideoRendering
 }
