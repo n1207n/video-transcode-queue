b3c93d4bdb59cfc4a618979b9852ff1f18e37706
Silin Na <n1207n@gmail.com>
2017-06-02 17:46:02 -0700

    feat: Created transcoder go module
    + Created ffmpeg cli command strings
    
    + Command subprocess runner

--- transcoder_service/transcode_api_server.go
+++ transcoder_service/transcode_api_server.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/golang/glog"
-	"github.com/nareix/joy4/av"
-	"github.com/nareix/joy4/av/avutil"
-	"github.com/nareix/joy4/av/transcode"
-	"github.com/nareix/joy4/cgo/ffmpeg"
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
@@ -37,13 +33,9 @@ func transcodeVideo(c *gin.Context) {
 	if c.BindJSON(&request) == nil {
 		// TODO: Check if video id exists in PostgreSQL
 
-		transcodedFilePaths, err := performTranscoding(request.Path)
+		performTranscoding(request.Path)
 
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("Failed to open a video file: %s for Video ID %s", request.Path, request.VideoID)})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"video_id": request.VideoID, "transcoded_list": transcodedFilePaths})
-		}
+		c.JSON(http.StatusAccepted, gin.H{"video_id": request.VideoID, "status": "In progress"})
 	}
 }
 
@@ -54,43 +46,17 @@ type TranscodeRequest struct {
 }
 
 func performTranscoding(filePath string) (transcodedFilePaths []string, transcodeError error) {
-	infile, err := avutil.Open(filePath)
+	splitStringPaths := strings.Split(filePath, "/")
+	fileFolderPath := strings.Join(splitStringPaths[:len(splitStringPaths)-1], "/")
+	filename := splitStringPaths[len(splitStringPaths)-1]
 
-	if err != nil {
-		transcodeError = err
-	}
+	// Strip the file extension and convert any reverse subsequent dots to underscore
+	splitFilenameCharacters := strings.Split(filename, ".")
+	videoName := strings.Join(splitFilenameCharacters[:len(splitFilenameCharacters)-1], "_")
 
-	encoded_audio_info := func(stream av.AudioCodecData, i int) (need bool, dec av.AudioDecoder, enc av.AudioEncoder, err error) {
-		need = true
-		dec, _ = ffmpeg.NewAudioDecoder(stream)
-		enc, _ = ffmpeg.NewAudioEncoderByName("libfdk_aac")
-		enc.SetSampleRate(stream.SampleRate())
-		enc.SetChannelLayout(av.CH_STEREO)
-		enc.SetBitrate(12000)
-		enc.SetOption("profile", "HE-AACv2")
-		return
-	}
-
-	transcode_demuxer := &transcode.Demuxer{
-		Options: transcode.Options{
-			FindAudioDecoderEncoder: encoded_audio_info,
-		},
-		Demuxer: infile,
-	}
-
-	outfile, err := avutil.Create("output.ts")
-
-	if err != nil {
-		transcodeError = err
-	} else {
-		avutil.CopyFile(outfile, transcode_demuxer)
-
-		transcodedFilePaths = append(transcodedFilePaths, "output.ts")
-
-		outfile.Close()
-		infile.Close()
-		transcode_demuxer.Close()
-	}
+	go TranscodeToStandard(videoName, filename, fileFolderPath)
+	go TranscodeToMobile(videoName, filename, fileFolderPath)
+	go TranscodeToHighSD(videoName, filename, fileFolderPath)
 
 	return
 }
---
+++ transcoder_service/transcoder.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/golang/glog"
+)
+
+// ExecuteFfmpegCLI executes constructed command string by os.exec.Command
+func ExecuteFfmpegCLI(commandString string) error {
+	commandArguments := strings.Fields(commandString)
+	head, commandArguments := commandArguments[0], commandArguments[1:]
+
+	cmd := exec.Command(head, commandArguments...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		glog.Errorf("Error during transcoding: %s\nError: %s", commandString, err.Error())
+		return err
+	}
+
+	return nil
+}
+
+// TranscodeToStandard transcodes a video file to Standard preset
+func TranscodeToStandard(videoName string, filename string, folderPath string) {
+	glog.Infof("Transcoding to Standard preset: %s\n", videoName)
+
+	ffmpegCommand480Pass1 := fmt.Sprintf("ffmpeg -i %s/%s -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=-1:480 -threads 0 -pass 1 -an -f mp4 /dev/null", folderPath, filename)
+
+	ExecuteFfmpegCLI(ffmpegCommand480Pass1)
+
+	// Standard web video (480p at 500kbit/s)
+	ffmpegCommand480Pass2 := fmt.Sprintf("ffmpeg -i %s/%s -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=-1:480 -threads 0 --pass 2 -codec:a libfdk_aac -b:a 128k -f mp4 %s/%s_480p.mp4", folderPath, filename, folderPath, videoName)
+
+	ExecuteFfmpegCLI(ffmpegCommand480Pass2)
+}
+
+// TranscodeToMobile transcodes video file to Mobile preset
+func TranscodeToMobile(videoName string, filename string, folderPath string) {
+	glog.Infof("Transcoding to Mobile preset: %s\n", videoName)
+
+	ffmpegCommand360Pass1 := fmt.Sprintf("ffmpeg -i %s/%s -codec:v libx264 -profile:v baseline -preset slow -b:v 250k -maxrate 250k -bufsize 500k -vf scale=-1:360 -threads 0 -pass 1 -an -f mp4 /dev/null", folderPath, filename)
+
+	ExecuteFfmpegCLI(ffmpegCommand360Pass1)
+
+	// 360p video for older mobile phones (360p at 250kbit/s in baseline profile)
+	ffmpegCommand360Pass2 := fmt.Sprintf("ffmpeg -i %s/%s -codec:v libx264 -profile:v baseline -preset slow -b:v 250k -maxrate 250k -bufsize 500k -vf scale=-1:360 -threads 0 --pass 2 -codec:a libfdk_aac -b:a 128k -f mp4 %s/%s_360p.mp4", folderPath, filename, folderPath, videoName)
+
+	ExecuteFfmpegCLI(ffmpegCommand360Pass2)
+}
+
+// TranscodeToHighSD transcodes video file to HighSD preset
+func TranscodeToHighSD(videoName string, filename string, folderPath string) {
+	glog.Infof("Transcoding to HighSD preset: %s\n", videoName)
+
+	ffmpegCommand576Pass1 := fmt.Sprintf("ffmpeg -i %s/%s -codec:v libx264 -profile:v high -preset slow -b:v 1000k -vf scale=-1:576 -threads 0 -pass 1 -an -f mp4 /dev/null", folderPath, filename)
+
+	ExecuteFfmpegCLI(ffmpegCommand576Pass1)
+
+	// High-quality SD video for archive/storage (PAL at 1Mbit/s in high profile):
+	ffmpegCommand576Pass2 := fmt.Sprintf("ffmpeg -i %s/%s -codec:v libx264 -profile:v high -preset slow -b:v 1000k -vf scale=-1:576 -threads 0 --pass 2 -codec:a libfdk_aac -b:a 196k -f mp4 %s/%s_576p.mp4", folderPath, filename, folderPath, videoName)
+
+	ExecuteFfmpegCLI(ffmpegCommand576Pass2)
+}
