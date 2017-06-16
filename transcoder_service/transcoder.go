0bf86c42a5993a29de901c84486944cd20b5807d
Silin Na <n1207n@gmail.com>
2017-06-16 00:37:47 -0700

    fix: Added overriding yes argument to ffmpeg

--- transcoder_service/transcoder.go
+++ transcoder_service/transcoder.go
@@ -35,7 +35,7 @@ func TranscodeToStandard(videoName string, filename string, folderPath string) {
 	ExecuteFfmpegCLI(ffmpegCommand480Pass1)
 
 	// Standard web video (480p at 500kbit/s)
-	ffmpegCommand480Pass2 := fmt.Sprintf("ffmpeg -i %s/%s -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=-2:480 -threads 0 -pass 2 -codec:a libfdk_aac -b:a 128k -f mp4 %s/%s_480p.mp4", folderPath, filename, folderPath, videoName)
+	ffmpegCommand480Pass2 := fmt.Sprintf("ffmpeg -y -i %s/%s -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=-2:480 -threads 0 -pass 2 -codec:a libfdk_aac -b:a 128k -f mp4 %s/%s_480p.mp4", folderPath, filename, folderPath, videoName)
 
 	ExecuteFfmpegCLI(ffmpegCommand480Pass2)
 }
@@ -49,7 +49,7 @@ func TranscodeToMobile(videoName string, filename string, folderPath string) {
 	ExecuteFfmpegCLI(ffmpegCommand360Pass1)
 
 	// 360p video for older mobile phones (360p at 250kbit/s in baseline profile)
-	ffmpegCommand360Pass2 := fmt.Sprintf("ffmpeg -i %s/%s -codec:v libx264 -profile:v baseline -level 3.1 -preset slow -b:v 250k -maxrate 250k -bufsize 500k -vf scale=-2:360 -threads 0 -pass 2 -codec:a libfdk_aac -b:a 128k -f mp4 %s/%s_360p.mp4", folderPath, filename, folderPath, videoName)
+	ffmpegCommand360Pass2 := fmt.Sprintf("ffmpeg -y -i %s/%s -codec:v libx264 -profile:v baseline -level 3.1 -preset slow -b:v 250k -maxrate 250k -bufsize 500k -vf scale=-2:360 -threads 0 -pass 2 -codec:a libfdk_aac -b:a 128k -f mp4 %s/%s_360p.mp4", folderPath, filename, folderPath, videoName)
 
 	ExecuteFfmpegCLI(ffmpegCommand360Pass2)
 }
@@ -63,7 +63,7 @@ func TranscodeToHighSD(videoName string, filename string, folderPath string) {
 	ExecuteFfmpegCLI(ffmpegCommand576Pass1)
 
 	// High-quality SD video for archive/storage (PAL at 1Mbit/s in high profile):
-	ffmpegCommand576Pass2 := fmt.Sprintf("ffmpeg -i %s/%s -codec:v libx264 -profile:v high -preset slow -b:v 1000k -vf scale=-2:576 -threads 0 -pass 2 -codec:a libfdk_aac -b:a 196k -f mp4 %s/%s_576p.mp4", folderPath, filename, folderPath, videoName)
+	ffmpegCommand576Pass2 := fmt.Sprintf("ffmpeg -y -i %s/%s -codec:v libx264 -profile:v high -preset slow -b:v 1000k -vf scale=-2:576 -threads 0 -pass 2 -codec:a libfdk_aac -b:a 196k -f mp4 %s/%s_576p.mp4", folderPath, filename, folderPath, videoName)
 
 	ExecuteFfmpegCLI(ffmpegCommand576Pass2)
 }
