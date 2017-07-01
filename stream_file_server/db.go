a22b2f46b76087d491d265ec84865a094e0135e2
Silin Na <n1207n@gmail.com>
2017-06-30 18:32:34 -0700

    fix: Improved UpdateVideoObject ORM method
    + Created stream_file_server service

---
+++ stream_file_server/db.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+// GetDatabaseConnection returns an instance
+// as a database connection
+func GetDatabaseConnection(user string, password string, host string, db string) *gorm.DB {
+	connection, err := gorm.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", user, password, host, db))
+
+	if err != nil {
+		panic(err)
+	}
+
+	return connection
+}
+
+// CreateSchemas creates a set of database tables
+// from Go struct classes
+func CreateSchemas(user string, password string, host string, db string) {
+	connection := GetDatabaseConnection(user, password, host, db)
+
+	defer connection.Close()
+
+	connection.AutoMigrate(&Video{}, &VideoRendering{})
+	connection.Model(&VideoRendering{}).AddForeignKey("video_id", "videos(id)", "CASCADE", "CASCADE")
+
+}
---
+++ stream_file_server/video.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Video represents an uploaded video instance
+// Relation:
+// - has many VideoRendering
+type Video struct {
+	ID        uint      `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time `gorm:"not null" json:"created_at"`
+	UpdatedAt time.Time `gorm:"not null" json:"updated_at"`
+
+	Title          string `gorm:"not null" json:"title" binding:"required"`
+	IsReadyToServe bool   `sql:"DEFAULT:false" json:"is_ready_to_serve"`
+	StreamFilePath string `json:"stream_file_path"`
+
+	Renderings []VideoRendering
+}
+
+func (v Video) String() string {
+	return fmt.Sprintf("Video: %d - %s", v.ID, v.Title)
+}
+
+// VideoRendering represents each rendering variant from original
+type VideoRendering struct {
+	ID             uint      `gorm:"primary_key" json:"id"`
+	CreatedAt      time.Time `gorm:"not null" json:"created_at"`
+	UpdatedAt      time.Time `gorm:"not null" json:"updated_at"`
+	RenderingTitle string    `gorm:"not null" json:"rendering_title" binding:"required"`
+
+	FilePath string `gorm:"not null" json:"file_path"`
+	URL      string `gorm:"not null" json:"url"`
+	Width    uint   `gorm:"not null" json:"width"`
+	Height   uint   `gorm:"not null" json:"height"`
+
+	VideoID uint `gorm:"index;not null"`
+}
+
+func (vr VideoRendering) String() string {
+	return fmt.Sprintf("VideoRendering: %d - %s", vr.ID, vr.RenderingTitle)
+}
